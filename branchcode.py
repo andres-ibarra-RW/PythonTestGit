1cfbb5f2d2f805f2b1dcd0eda3aa97faa1c84454
andres-ibarra-RW <andres.ibarra@rulesware.com>
2022-03-28 17:39:11 -0600

    added list and loop

--- branchcode.py
+++ branchcode.py
@@ -2,3 +2,11 @@ print("this is a test for branching")
 
 
 print("this is on loopbranch")
+
+#this will be added on the new file with out modifing the code before
+
+l = list()
+for i in range(6):
+    l.append(i)
+
+print(l)
